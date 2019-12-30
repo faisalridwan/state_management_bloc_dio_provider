fdcfcd594dcd0e5bd22aa91cebcfb8f42baa1fd9
Faisal Ridwan <faisalridwansiregar@gmail.com>
2019-12-30 14:27:55 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:state_management_app/blocs/all_post_bloc.dart';
+import 'package:state_management_app/pages/home_page.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(builder: (_) => AllPostBloc()),
+      ],
+      child: MaterialApp(
+        title: 'State Management',
+        theme: ThemeData(
+          primaryColor: Colors.red,
+        ),
+        home: HomePage(),
+      ),
+    );
+  }
+}
